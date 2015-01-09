34bd33014fb292d4dfd2137e33a668d3ea10875f
Karen <karenpenglabs@gmail.com>
2015-01-08 23:53:51 -0800

    hey python is weird

--- mergeSort.py
+++ mergeSort.py
@@ -12,15 +12,19 @@ def combine(arr1, arr2):
   i = j = 0
   index = 0
   arr3 = []
-  while(index < len(arr1) + len(arr2)):
+  while(i < len(arr1)-1 || j < len(arr2)-1 ):
     if(arr1[i] >= arr2[j]):
       arr3[index] = arr2[j]
-      j++
-      index++
-    else
+      j = j+1
+      index=index+1
+    else:
       arr3[index] = arr1[i]
-      i++
-      index++
+      i = i+1
+      index = index +1
+    for num in arr3:
+      print num
   return arr3
 
-
+test1 = [ 4,6,13,6,2,7]
+test2 = [5,3,135,3]
+combine(test1, test2)
