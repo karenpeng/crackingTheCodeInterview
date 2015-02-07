a3f590279f7d4d758b078ee59c1934a315546eb4
Karen <karenpenglabs@gmail.com>
2015-02-06 23:43:35 -0500

    nope

--- search/binarySearch/binarySearch.py
+++ search/binarySearch/binarySearch.py
@@ -1,15 +1,20 @@
-def binarySearch(aList, targetValue):
-    min = 0
-    max = len(aList) - 1
-    guess = (min + max) / 2
+def binarySearch(aList, min, max, targetValue):
 
-    print guess
+    print min
+    print '~~~'
+    print max
+    print '\n----------\n'
 
-    if max < 1:
+    if max < min:
 
+        print 'end'
         return -1
 
     else:
+
+        guess = (min + max) / 2
+
+        print guess
 
         if aList[guess] == targetValue:
 
@@ -17,15 +22,17 @@ def binarySearch(aList, targetValue):
 
         elif aList[guess] > targetValue:
 
-            aList = aList[min: guess - 1:]
+            #aList = aList[min: guess - 1:]
+            max = guess - 1
 
         else:
 
-            aList = aList[guess + 1: max:]
+            #aList = aList[guess + 1: max:]
+            min = guess + 1
 
-        binarySearch(aList, targetValue)
+        binarySearch(aList, min, max, targetValue)
 
 
 test4 = [2, 2, 4, 6, 6, 6, 7, 13, 15, 64, 64, 246, 346]
 
-print binarySearch(test4, 6)
+print binarySearch(test4, 0, len(test4) - 1, 6)
