fdd27e53ca943dae09671535e7fc5bc2c9362f71
Karen <karenpenglabs@gmail.com>
2015-06-04 22:22:58 -0400

    要跪了

--- linked_list/reverse_linkedlistII.js
+++ linked_list/reverse_linkedlistII.js
@@ -9,6 +9,10 @@ var c = new Node(3, d)
 var b = new Node(2, c)
 var a = new Node(1, b)
 
+c = new Node(3, null)
+b = new Node(2, c)
+a = new Node(1, b)
+
 function print(head){
   n = head
   while(n !== null){
@@ -18,66 +22,53 @@ function print(head){
 }
 
 function reverse(head, m, n){
-   if(head === null || head.next === null || head.next.next === null) return head
-  // 0->1->2->3->4->5->null
-  // 
-  // 走m步 记住0和1 然后走n-m步 记住3和4
-  // 然后把123翻转
-  // 然后把0连去3  1连去4
+   if(head === null || n <= m) return head
+  // 1->2->3->4->5->null
+  // 1->4->3->2->5->null
+  // 走m步 记住1和2 然后走n-m步 记住4和5
+  // 然后把234翻转
+  // 然后把1连去4  2连去5
   var headHead = head
-  
-  var beginCount = 0
   var endOfFisrtPart = head
-
-  while(beginCount < m-1){
+  
+  for(var i = 1; i < m - 1; i++){//because i want the one 
+    //that is in front of the end
+    //so m - 1
+    if(endOfFisrtPart.next === null) return headHead
     endOfFisrtPart = endOfFisrtPart.next
-    beginCount++
   }
 
-  console.log('~~~~~1st')
-  console.log(endOfFisrtPart, beginCount, m)
-  console.log('----')
+  var startOfThirdPart = head
 
-  var endCount = 0
-  var startOfThirdPart = endOfFisrtPart
-  console.log(m, n, endCount)
-
-  while(endCount < (m - n) ){
-    console.log('ouch')
-    //startOfThirdPart = startOfThirdPart.next
-    // console.log('((')
-    // console.log(startOfThirdPart)
-    // console.log('))')
-    endCount++
+  for(var j = 1; j < n + 1; j++){//because i want the one 
+    //that is behind the end
+    //so n + 1
+    if(startOfThirdPart.next === null) return headHead
+    startOfThirdPart = startOfThirdPart.next 
   }
 
-  // console.log('~~~~~3rd')
-  // console.log(startOfThirdPart, endCount, n)
-  // console.log('----')
+  var curt = endOfFisrtPart.next
+  var prev = startOfThirdPart
+  var temp = curt.next
 
-  // var head = endOfFisrtPart.next
-  // var prev = startOfThirdPart
-  // var temp = head.next
-  //console.log(head, '-', prev, '-', temp)
+  do{
+    curt.next = prev
+    prev = curt
+    curt = temp
+    temp = temp.next//null啊 要跪了 T_T ！！！
+  }while(temp !== startOfThirdPart)
 
-  // while(temp.next !== startOfThirdPart){
-  //   head.next = prev
-  //   prev = head
-  //   head = temp
-  //   temp = temp.next
-  //   console.log(temp)
-  //   console.log('-')
-  // }
-  // head.next = prev
+  curt.next = prev
+  endOfFisrtPart.next = curt
 
-  // endOfFisrtPart.next = head
-
-  // return headHead
+  return headHead
 
 }
 
-//print(a)
-//console.log('---')
+
+print(a)
+console.log('---')
 //print(reverse(d))
-reverse(a, 2, 4)
+print(reverse(a, 1, 2))
+//print(reverse(a, 2, 4))
 
