cc87f9ac27a2ec60df45b4b10bce59699dcf1d42
Karen <karenpenglabs@gmail.com>
2015-07-06 23:26:15 -0400

    i did it:)

--- linked_list/mergekList.js
+++ linked_list/mergekList.js
@@ -9,26 +9,6 @@
  * @param {ListNode[]} lists
  * @return {ListNode}
  */
-var mergeKLists = function(lists) {
-    if(lists.length < 1) return null
-    //全部合起来然后一半一半
-    //divide and conquer
-    return helper(0, lists.length-1, lists)
-    
-};
-
-function helper(start, end, lists){
-    if(start === end){
-        return lists[start]
-    }
-    
-    var mid = Math.floor((start + end) / 2)
-    var l = helper(start, mid, lists)
-    var r = helper(mid+1, end, lists)
-    
-    return mergeTwoList(l, r)
-    
-}
 
 function mergeTwoList(list1, list2){
     var dummy = new ListNode(null)
@@ -55,3 +35,63 @@ function mergeTwoList(list1, list2){
     }
     return head.next
 }
+
+
+//1.bottom to up
+var mergeKLists = function(lists) {
+    if(lists.length < 1) return null
+    //全部合起来然后一半一半
+    //divide and conquer
+    return helper(0, lists.length-1, lists)
+    
+};
+
+function helper(start, end, lists){
+    if(start === end){
+        return lists[start]
+    }
+    
+    var mid = Math.floor((start + end) / 2)
+    var l = helper(start, mid, lists)
+    var r = helper(mid+1, end, lists)
+    
+    return mergeTwoList(l, r)
+    
+}
+
+//2.up to bottom
+var mergeKLists2 = function(lists) {
+    if(lists.length < 1) return null
+    return helper2(lists)
+}
+
+function helper2(arr){
+    if(arr.length ===1) return arr[0]
+    var level = []
+    var i = 0
+    while(i<arr.length-1){
+        level.push(mergeTwoList(arr[i], arr[i+1]))
+        i+=2
+    }
+    if(arr.length % 2 === 1) level.push(arr[arr.length-1])
+    
+    return helper2(level)
+}
+
+
+//方法三
+var mergeKLists3 = function(lists) {
+    if(lists.length < 1) return null
+    while(lists.length > 1){
+        var level = []
+        var i = 0
+        while(i<lists.length-1){
+            level.push(mergeTwoList(lists[i], lists[i+1]))
+            i+=2
+        }
+        if(lists.length % 2 === 1) level.push(lists[lists.length-1])
+        lists = level
+    }
+    return lists[0]
+}
+
