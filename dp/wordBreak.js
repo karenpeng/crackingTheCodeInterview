0e7a5bfa8df2416960b4395ec4b56adc5f14413d
Karen <karenpenglabs@gmail.com>
2015-08-07 22:46:53 -0400

    im a new person today

--- dp/wordBreak.js
+++ dp/wordBreak.js
@@ -48,24 +48,58 @@
 // // console.log(wordBreak('a', new Set(['a'])))
 // console.log(wordBreak('ab', new Set(['a', 'b'])))
 
+// var wordBreak = function(s, wordDict) {
+//  var f = []
+//  f[-1] = true
+//  for(var i = 0; i < s.length; i++){
+//      f[i] = false
+//      for(var j = -1; j < i; j ++){
+//       //console.log(s.substring(j+1, i))
+//          if(f[j] && wordDict.has(s.substring(j+1, i+1))) {
+//              f[i] = true
+//              break
+//          }
+//      }
+//  }
+ 
+//  return f[s.length - 1]
+// }
+/**
+ * @param {string} s
+ * @param {set<string>} wordDict
+ * @return {boolean}
+ */
+// state: f[i]表示前i个字符能否被完美切分
+// function: f[i] = OR{f[j]}, j < i, j+1 ~ i是一个词
+// 典中的单词
+// intialize: f[0] = true
+// answer: f[s.length()]
+// 注意：切分位置的枚举->单词长度枚举
+// O(NL), N: 字符串长度, L: 最长的单词的长度
 var wordBreak = function(s, wordDict) {
  var f = []
- f[-1] = true
- for(var i = 0; i < s.length; i++){
+ f[0] = true
+ var max = 0
+ 
+ wordDict.forEach(function(w){
+     max = Math.max(max, w.length)
+ })
+ 
+ for(var i = 1; i <= s.length; i++){
      f[i] = false
-     for(var j = -1; j < i; j ++){
-      //console.log(s.substring(j+1, i))
-         if(f[j] && wordDict.has(s.substring(j+1, i+1))) {
+     for(var j = i; j >= i - max; j--){
+         if(f[j] && wordDict.has(s.substring(j, i))){
              f[i] = true
              break
          }
      }
  }
- 
- return f[s.length - 1]
+ return f[s.length]
 }
 
-console.log(wordBreak('ab', new Set(['a', 'b'])))
+console.log(wordBreak('abcdefg', new Set(['a', 'b', 'c', 'd', 'e', 'f', 'g'])))
+//console.log(wordBreak('ab', new Set(['a', 'b'])))
+//console.log(wordBreak('a', new Set([])))
 
 //注意substring的使用方法：
 //a = 'adggdfg'
--- experiments/js/fib.js
+++ experiments/js/fib.js
@@ -3,21 +3,34 @@ function fib1(n){
   return fib1(n-1) + fib1(n-2)
 }
 
-function fib2(n){
-  if(n === 0 || n === 1) return n
-  var pre0 = 1
-  var pre1 = 1
-  for(var i = 2; i < n; i++){
-    var tem = pre1
-    pre1 += pre0
-    pre0 = tem
-  }
-  return pre0
-}
-
 var hash = {}
-function fib3(n){
+function fib2(n){
   if(n === 0 || n === 1) return n
   if(!hash.hasOwnProperty(n)) hash[n] = fib3(n-1) + fib3(n-2)
   return hash[n]
 }
+
+function fib3(_n){
+  var f = []
+  f[0] = 0
+  f[1] = 1
+  for(var n = 2; n <= _n; n++){
+    f[n] = f[n-1] + f[n-2]
+  }
+  return f[_n]
+}
+
+function fib4(n){
+  if(n===0||n===1) return n
+  var a = 0
+  var b = 1
+  for(var i = 2; i <= n; i++){
+    var c = a + b
+    a = b
+    b = c
+  }
+  return c
+}
+
+console.log(fib3(100))
+console.log(fib4(100))
